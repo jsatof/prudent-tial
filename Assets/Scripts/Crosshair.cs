2c944cdbfba8af0ecd00f6ba22b247b74ca36d25
jsferrarelli <jsferr4@gmail.com>
2021-08-04 00:20:16 -0400

    Movement of cursor good enough

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour {
     public Transform movePoint;
-
+	public float moveSpeed = 5f;
 
 	void Start() {
     	transform.position = new Vector3(-1.5f, -1.5f, 0f);
@@ -12,17 +12,22 @@ public class Crosshair : MonoBehaviour {
     }
 
     void Update() {
-    	if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-			Vector3 moveFactor = new Vector3(1.5f, 0f, 0f);
-			Vector3 move = moveFactor * Input.GetAxisRaw("Horizontal");
-			transform.position += move;
-		}
+    	transform.position = Vector3.MoveTowards(transform.position, movePoint.position, Time.deltaTime * moveSpeed);
+
+		if(Vector3.Distance(transform.position, movePoint.position) <= 0.5f) {
+
+			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+				Vector3 moveFactor = new Vector3(1f, 0f, 0f);
+				Vector3 move = moveFactor * Input.GetAxisRaw("Horizontal");
+				movePoint.position += move;
+			}
 
 	 
-    	if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-			Vector3 moveFactor = new Vector3(0f, 1.5f, 0f);
-			Vector3 move = moveFactor * Input.GetAxisRaw("Vertical");
-			transform.position += move;
+    		if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+				Vector3 moveFactor = new Vector3(0f, 1f, 0f);
+				Vector3 move = moveFactor * Input.GetAxisRaw("Vertical");
+				movePoint.position += move;
+			}
 		}
     }
 }
