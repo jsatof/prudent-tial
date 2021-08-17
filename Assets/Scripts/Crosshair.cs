08a89e4193ca47fdaf5bf062c51988bbe73aadb1
jsferrarelli <jsferr4@gmail.com>
2021-08-17 14:24:43 -0400

    Disabled diagonal cursor movement. Can only move cursor once move animation is finished

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Crosshair : MonoBehaviour {
     public Transform movePoint;
 	public float moveSpeed = 4f;
+	public Tile hoveredTile;
 
 	void Start() {
     	transform.position = new Vector3(-1.5f, -1.5f, 0f);
@@ -13,21 +15,31 @@ public class Crosshair : MonoBehaviour {
 
     void Update() {
     	transform.position = Vector3.MoveTowards(transform.position, movePoint.position, Time.deltaTime * moveSpeed);
+		
+	//	hoveredTile = World.Tile((int)transform.position.x, (int)transform.position.y);
 
-		if(Vector3.Distance(transform.position, movePoint.position) <= 0.5f) {
-
-			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+		// Check Arrow Keys: Moves cursor
+		if(Vector3.Distance(transform.position, movePoint.position) == 0f) {
+			
+			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) != 1f) { // disables diagonal movement
 				Vector3 moveFactor = new Vector3(1f, 0f, 0f);
 				Vector3 move = moveFactor * Input.GetAxisRaw("Horizontal");
 				movePoint.position += move;
 			}
-
 	 
-    		if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+    		if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 1f) {
 				Vector3 moveFactor = new Vector3(0f, 1f, 0f);
 				Vector3 move = moveFactor * Input.GetAxisRaw("Vertical");
 				movePoint.position += move;
 			}
 		}
+
+		// Check Mouse Clicks: Left Click: Select, Right Click: Back
+		if(Input.GetMouseButtonDown(0)) { // Left Mouse Click
+			// send data to click manager
+		}
+
+
+
     }
 }
