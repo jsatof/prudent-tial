ce2fb190a4da5e2761cac799d0d7be261fd2a219
jsferrarelli <jsferr4@gmail.com>
2021-08-18 17:29:58 -0400

    Add hud canvas for unit stats

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats {
+	public int attackStat;
+	public int defenseStat;
+	public int luckStat;
+	public int currentExp;
+	public int maxExp;
+	public int level;
+	public int currentHp;
+	public int maxHp;
+}
--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -5,21 +5,40 @@ using UnityEngine.Tilemaps;
 
 public class ClickManager : MonoBehaviour {
 	private GameObject cursor;
-	private Tilemap tilemap;
+	private GameObject protag;
+	private GameObject enemy1;
+	private List<GameObject> sprites;
+
+	public GameObject selected;
 
 	void Start() {
 		cursor = GameObject.Find("Crosshair");
+		selected = null;
+
+		protag = GameObject.Find("ProtagSprite");
+		sprites.Add(protag);
+
+		enemy1 = GameObject.Find("EnemySprite");
+		sprites.Add(enemy1);
     }
 
     void Update() {
-		Ray castPoint = Camera.main.ScreenPointToRay(cursor.transform.position);
-		RaycastHit hit;
-		if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))  {
-		//	objectToMove.transform.position = hit.point;
-        }
-		Debug.Log(cursor.transform.position);
-		//Vector3Int gPos = grid.WorldToCell(cursor.transform.position);
-		//Debug.Log("name: " + tilemap.GetTile(gPos).name + "\nPosition: " + gPos);
-    }
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			GameObject match = checkCursorAndSpritePos();
+			if(match) {
+				selected = match;
+			}
+		}
 
+    }
+	
+
+	private GameObject checkCursorAndSpritePos() {
+		foreach(GameObject sprite in sprites) {
+			if(sprite.transform.position == cursor.transform.position) {
+				return sprite;
+			}
+		}
+		return null;
+	}
 }
