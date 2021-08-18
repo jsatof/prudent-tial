aa182eca53e95e013f14582a87b193e777132905
jsferrarelli <jsferr4@gmail.com>
2021-08-17 20:53:47 -0400

    Added more tiles to fill screen. Tried organizing tiles into easy to access grid - didnt work too hot

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -16,30 +16,38 @@ public class Crosshair : MonoBehaviour {
     void Update() {
     	transform.position = Vector3.MoveTowards(transform.position, movePoint.position, Time.deltaTime * moveSpeed);
 		
-	//	hoveredTile = World.Tile((int)transform.position.x, (int)transform.position.y);
-
 		// Check Arrow Keys: Moves cursor
-		if(Vector3.Distance(transform.position, movePoint.position) == 0f) {
-			
-			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) != 1f) { // disables diagonal movement
-				Vector3 moveFactor = new Vector3(1f, 0f, 0f);
-				Vector3 move = moveFactor * Input.GetAxisRaw("Horizontal");
-				movePoint.position += move;
-			}
+		if(Vector3.Distance(transform.position, movePoint.position) == 0f) {  // would call IsInMapBounds() here
+			MoveLeftRight();
+			MoveUpDown();
 	 
-    		if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 1f) {
-				Vector3 moveFactor = new Vector3(0f, 1f, 0f);
-				Vector3 move = moveFactor * Input.GetAxisRaw("Vertical");
-				movePoint.position += move;
-			}
 		}
-
-		// Check Mouse Clicks: Left Click: Select, Right Click: Back
-		if(Input.GetMouseButtonDown(0)) { // Left Mouse Click
-			// send data to click manager
-		}
-
-
 
     }
+
+	private void MoveLeftRight() {
+		if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) != 1f) { // disables diagonal movement
+			Vector3 moveFactor = new Vector3(1f, 0f, 0f);
+			Vector3 move = moveFactor * Input.GetAxisRaw("Horizontal");
+			movePoint.position += move;
+		}
+	}
+
+	private void MoveUpDown() {
+    	if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 1f) {
+			Vector3 moveFactor = new Vector3(0f, 1f, 0f);
+			Vector3 move = moveFactor * Input.GetAxisRaw("Vertical");
+			movePoint.position += move;
+		}
+	}
+
+	// BUG: cursor would advance one space over bounds, then be locked in place
+	private bool IsInMapBounds() {
+		if(movePoint.position.x >= -11.5 && movePoint.position.x <= 11.5) {
+			if(movePoint.position.y >= -6.5 && movePoint.position.y <= 4.5) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
